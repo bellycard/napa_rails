138e3d0ae5691bab9cac80bc037759a4571184c5
Flori Garcia Vicente <florigarciavicente@Floris-Air-3.local>
2016-04-11 15:14:28 -0500

    initial commit

---
+++ lib/napa_rails.rb
@@ -0,0 +1,5 @@
+require "napa_rails/version"
+
+module NapaRails
+  # Your code goes here...
+end
---
+++ spec/napa_rails_spec.rb
@@ -0,0 +1,11 @@
+require 'spec_helper'
+
+describe NapaRails do
+  it 'has a version number' do
+    expect(NapaRails::VERSION).not_to be nil
+  end
+
+  it 'does something useful' do
+    expect(false).to eq(true)
+  end
+end
