4564a7381db56c0ff35f2555d11ae880e8a99de5
Jon Homan <jhoman@bellycard.com>
2016-07-28 13:53:54 -0500

    add SortableApi
    The SubCategories API in business-service uses this module. Porting from
    napa.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,2 +1,10 @@
+require 'active_record'
+
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'napa'
+
+require 'acts_as_fu'
+
+RSpec.configure do |config|
+  config.include ActsAsFu
+end
